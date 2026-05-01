6a3839d36d63afb28885e7f8a75f56d0bfe559eb
meryembenassila <benassila.meryem08@gmail.com>
2026-05-01 10:23:51 +0100

    intial commit

---
+++ src/main/java/com/example/appbanquaire/AppBanquaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.appbanquaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppBanquaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppBanquaireApplication.class, args);
+    }
+
+}
